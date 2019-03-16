764cd8e70a745f61f66254b1e8a53f049376ae87
Martin Lindahl <martin_lindahl9@hotmail.com>
2019-03-16 16:57:02 +0100

    Integrated twilio api

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const restify = require('restify')
 require('dotenv').config()
+const { PORT, accountSid, authToken } = process.env
 
-const { PORT } = process.env
+const client = require('twilio')(accountSid, authToken)
 
 const server = restify.createServer()
+server.use(restify.plugins.bodyParser())
 
 server.post('/text', (req, res, next) => {
     console.log('recieved a POST!')
+
+    client.messages
+        .create({
+            body: 'Test message from SMS-app',
+            from: 'SMS-App',
+            from: '+46769448149',
+            to: '+46707486474'
+        })
+        .then((message) => {
+            console.log('Message sent:')
+            console.log(message)
+        })
+
     res.send(200)
     next()
 })
